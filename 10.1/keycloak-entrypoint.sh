2984ecb013b80407bcbfced1a2d696f94dabacdb
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-06-01 10:42:20 -0500

    add client registration

--- 10.1/keycloak-entrypoint.sh
+++ 10.1/keycloak-entrypoint.sh
@@ -87,4 +87,14 @@ fi
 
 rm -rf /wildfly/standalone/configuration/standalone_xml_history/current/*
 
+# Client registration
+if [ -n "$KEYCLOAK_INITIAL_ACCESS_TOKEN" ] && [ -n "$KEYCLOAK_AUTH_SERVER_URL" ] && [ -n "$KEYCLOAK_REALM" ] && [ -n "$KEYCLOAK_RESOURCE" ]
+then
+  /wildfly/bin/kcreg.sh config initial-token $KEYCLOAK_INITIAL_ACCESS_TOKEN --server $KEYCLOAK_AUTH_SERVER_URL --realm $KEYCLOAK_REALM
+  /wildfly/bin/kcreg.sh create -s clientId=$KEYCLOAK_RESOURCE -s protocol=openid-connect -s rootUrl=/$KEYCLOAK_RESOURCE
+  if [ -n "$KEYCLOAK_BEARER_ONLY" ]; then
+    /wildfly/bin/kcreg.sh update $KEYCLOAK_RESOURCE -s bearerOnly=$KEYCLOAK_BEARER_ONLY
+  fi  
+fi
+
 exec "$@"
