8b7272ea60aeba3891866c46e1a191960efba0f8
carlosthe19916@gmail.com <carlosthe19916@gmail.com>
2017-06-01 12:29:18 -0500

    resolve subsystem

--- 10.1/keycloak-entrypoint.sh
+++ 10.1/keycloak-entrypoint.sh
@@ -18,70 +18,72 @@ then
     /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/realm=$KEYCLOAK_REALM:write-attribute\(name=public-key,value=$KEYCLOAK_PUBLIC_KEY\)
   fi
   if [ -n "$KEYCLOAK_SSL_REQUIRED" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/realm=$KEYCLOAK_REALM:write-attribute\(ssl-required,value=$KEYCLOAK_SSL_REQUIRED\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/realm=$KEYCLOAK_REALM:write-attribute\(name=ssl-required,value=$KEYCLOAK_SSL_REQUIRED\)
   fi
 
   # Add secure deployment
+  /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(realm=$KEYCLOAK_REALM\)
+  
   if [ -n "$KEYCLOAK_RESOURCE" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(resource=$KEYCLOAK_RESOURCE\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=resource,value=$KEYCLOAK_RESOURCE\)
   fi
   if [ -n "$KEYCLOAK_USE_RESOURCE_ROLE_MAPPINGS" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(use-resource-role-mappings=$KEYCLOAK_USE_RESOURCE_ROLE_MAPPINGS\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=use-resource-role-mappings,value=$KEYCLOAK_USE_RESOURCE_ROLE_MAPPINGS\)
   fi
   if [ -n "$KEYCLOAK_ENABLE_CORS" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(enable-cors=$KEYCLOAK_ENABLE_CORS\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=enable-cors,value=$KEYCLOAK_ENABLE_CORS\)
   fi
   if [ -n "$KEYCLOAK_CORS_MAX_AGE" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(cors-max-age=$KEYCLOAK_CORS_MAX_AGE\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=cors-max-age,value=$KEYCLOAK_CORS_MAX_AGE\)
   fi
   if [ -n "$KEYCLOAK_CORS_ALLOWED_METHODS" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(cors-allowed-methods=$KEYCLOAK_CORS_ALLOWED_METHODS\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=cors-allowed-methods,value=$KEYCLOAK_CORS_ALLOWED_METHODS\)
   fi
   if [ -n "$KEYCLOAK_BEARER_ONLY" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(bearer-only=$KEYCLOAK_BEARER_ONLY\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=bearer-only,value=$KEYCLOAK_BEARER_ONLY\)
   fi
   if [ -n "$KEYCLOAK_ENABLE_BASIC_AUTH" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(enable-basic-auth=$KEYCLOAK_ENABLE_BASIC_AUTH\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=enable-basic-auth,value=$KEYCLOAK_ENABLE_BASIC_AUTH\)
   fi
   if [ -n "$KEYCLOAK_EXPOSE_TOKEN" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(expose-token=$KEYCLOAK_EXPOSE_TOKEN\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=expose-token,value=$KEYCLOAK_EXPOSE_TOKEN\)
   fi
   if [ -n "$KEYCLOAK_CREDENTIAL" ]; then
     /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME/credential=secret:add\(value=$KEYCLOAK_CREDENTIAL\)
   fi
 
   if [ -n "$KEYCLOAK_CONNECTION_POOL_SIZE" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(connection-pool-size=$KEYCLOAK_CONNECTION_POOL_SIZE\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=connection-pool-size,value=$KEYCLOAK_CONNECTION_POOL_SIZE\)
   fi
   if [ -n "$KEYCLOAK_DISABLE_TRUST_MANAGER" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(disable-trust-manager=$KEYCLOAK_DISABLE_TRUST_MANAGER\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=disable-trust-manager,value=$KEYCLOAK_DISABLE_TRUST_MANAGER\)
   fi
   if [ -n "$KEYCLOAK_ALLOW_ANY_HOSTNAME" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(allow-any-hostname=$KEYCLOAK_ALLOW_ANY_HOSTNAME\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=allow-any-hostname,value=$KEYCLOAK_ALLOW_ANY_HOSTNAME\)
   fi
   if [ -n "$KEYCLOAK_TRUSTSTORE" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(truststore=$KEYCLOAK_TRUSTSTORE\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=truststore,value=$KEYCLOAK_TRUSTSTORE\)
   fi
   if [ -n "$KEYCLOAK_TRUSTSTORE_PASSWORD" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(truststore-password=$KEYCLOAK_TRUSTSTORE_PASSWORD\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=truststore-password,value=$KEYCLOAK_TRUSTSTORE_PASSWORD\)
   fi
   if [ -n "$KEYCLOAK_CLIENT_KEYSTORE" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(client-keystore=$KEYCLOAK_CLIENT_KEYSTORE\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=client-keystore,value=$KEYCLOAK_CLIENT_KEYSTORE\)
   fi
   if [ -n "$KEYCLOAK_CLIENT_KEYSTORE_PASSWORD" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(client-keystore-password=$KEYCLOAK_CLIENT_KEYSTORE_PASSWORD\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=client-keystore-password,value=$KEYCLOAK_CLIENT_KEYSTORE_PASSWORD\)
   fi
   if [ -n "$KEYCLOAK_CLIENT_KEY_PASSWORD" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(client-key-password=$KEYCLOAK_CLIENT_KEY_PASSWORD\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=client-key-password,value=$KEYCLOAK_CLIENT_KEY_PASSWORD\)
   fi
   if [ -n "$KEYCLOAK_TOKEN_MINIMUN_TIME_TO_LIVE" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(token-minimun-time-to-live=$KEYCLOAK_TOKEN_MINIMUN_TIME_TO_LIVE\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=token-minimun-time-to-live,value=$KEYCLOAK_TOKEN_MINIMUN_TIME_TO_LIVE\)
   fi
   if [ -n "$KEYCLOAK_MIN_TIME_BETWEEN_JWKS_REQUESTS" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(min-time-between-jwks-requests=$KEYCLOAK_MIN_TIME_BETWEEN_JWKS_REQUESTS\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=min-time-between-jwks-requests,value=$KEYCLOAK_MIN_TIME_BETWEEN_JWKS_REQUESTS\)
   fi
   if [ -n "$KEYCLOAK_PUBLIC_KEY_CACHE_TTL" ]; then
-    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:add\(public-key-cache-ttl=$KEYCLOAK_PUBLIC_KEY_CACHE_TTL\)
+    /wildfly/bin/jboss-cli.sh --commands=embed-server,/subsystem=keycloak/secure-deployment=$WAR_NAME:write-attribute\(name=public-key-cache-ttl,value=$KEYCLOAK_PUBLIC_KEY_CACHE_TTL\)
   fi
 fi
 
